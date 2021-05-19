9c95a278fe82aaf8914cd7fc7756fd26cf658666
henosch <59366586+henosch@users.noreply.github.com>
2021-05-19 18:33:12 +0200

    update 02_install.sh

--- 02_install.sh
+++ 02_install.sh
@@ -1,1 +1,94 @@
-Fgg
+#!/bin/bash
+
+[ "$UID" -eq 0 ] || exec sudo bash "$0" "$@"
+
+# error fix: udisksd[433]: failed to load module mdraid: libbd_mdraid.so.2
+apt install libblockdev-mdraid2
+
+
+# install awesome vim current user
+git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
+sh ~/.vim_runtime/install_awesome_vimrc.sh
+
+  
+###########################
+#        Install any php Version             #
+###########################
+
+# php 5.6
+apt install php5.6-fpm php5.6-gd php5.6-mysql php5.6-curl php5.6-xml \
+  php5.6-zip php5.6-intl libapache2-mod-php5.6 php5.6-mbstring php5.6-json \
+  php5.6-bz2 php5.6 php5.6-cli php5.6-common php5.6-ssh2 php5.6-mcrypt php5.6-sqlite3 \
+  php5.6-bcmath php5.6-gmp
+
+# php 7.0  
+apt install php7.0-fpm php7.0-gd php7.0-mysql php7.0-curl php7.0-xml \
+  php7.0-zip php7.0-intl libapache2-mod-php7.0 php7.0-mbstring php7.0-json \
+  php7.0-bz2 php7.0 php7.0-cli php7.0-common php7.0-ssh2 php7.0-mcrypt php7.0-sqlite3 \
+  php7.0-bcmath php7.0-gmp
+
+# php 7.3
+apt install php7.3-fpm php7.3-gd php7.3-mysql php7.3-curl php7.3-xml \
+  php7.3-zip php7.3-intl libapache2-mod-php7.3 php7.3-mbstring php7.3-json \
+  php7.3-bz2 php7.3 php7.3-cli php7.3-common php7.3-ssh2 php7.3-sqlite3 \
+  php7.3-bcmath php7.3-gmp
+
+# php 7.4
+apt install php7.4-fpm php7.4-gd php7.4-mysql php7.4-curl php7.4-xml \
+  php7.4-zip php7.4-intl libapache2-mod-php7.4 php7.4-mbstring php7.4-json \
+  php7.4-bz2 php7.4 php7.4-cli php7.4-common php7.4-sqlite3 php7.4-bcmath php7.4-gmp
+
+systemctl enable php5.6-fpm php7.0-fpm php7.3-fpm
+systemctl enable php7.4-fpm
+
+# standard php version for apache is 7.0
+a2dismod php7.3 php5.6 php7.4
+a2enmod  php7.0
+systemctl restart apache2
+
+# standard php version for system
+update-alternatives --set php /usr/bin/php7.3
+php -i | grep "Loaded Configuration File"
+
+
+###########################
+# Install Xserver on rasp Image lite   #
+###########################
+
+apt install --no-install-recommends xserver-xorg
+apt install raspberrypi-ui-mods xinit firefox-esr-l10n-de piclone
+
+# error fix: Error getting user list from org.freedesktop.Accounts: GDBus.Error
+apt install accountsservice
+
+# error fix
+mkdir /var/lib/lightdm/data
+chown lightdm:lightdm /var/lib/lightdm/data
+
+# full xserver
+# apt install lxde lxde-core lxterminal lxappearanextcloude lightdm raspberrypi-ui-mods
+
+
+#############################
+#    xrdp - Microsoft Remote Desktop    #
+#############################
+
+apt install xrdp
+service xrdp start
+update-rc.d xrdp enable
+
+
+######################
+#      Install VNC Server            #
+######################
+
+apt install acl bc colord colord-data cups cups-browsed cups-client cups-common \
+  cups-core-drivers cups-daemon cups-filters cups-filters-core-drivers \
+  cups-ipp-utils cups-ppdc cups-server-common libcolorhug2 libfontembed1 libgusb2 \
+  libgutenprint-common libgutenprint9 libieee1284-3 liblouis-data liblouis17 \
+  liblouisutdml-bin liblouisutdml-data liblouisutdml8 libpoppler82 libqpdf21 \
+  libsane libsane-common libyaml-0-2 poppler-utils printer-driver-gutenprint \
+  realvnc-vnc-server sane-utils update-inetd
+
+systemctl enable vncserver-x11-serviced.service
+systemctl start vncserver-x11-serviced.service
