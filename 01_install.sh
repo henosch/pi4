9655ef3c366091b52a5bc618cf8727958d0e5c42
henosch <59366586+henosch@users.noreply.github.com>
2021-05-20 03:44:29 +0200

    update 01_install.sh

--- 01_install.sh
+++ 01_install.sh
@@ -10,6 +10,20 @@ sed -i 's/NOPASSWD/PASSWD/g' /etc/sudoers.d/010_pi-nopasswd
 echo "mike ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers.d/010_pi-nopasswd
 
 
+#################
+#      sshd settings        #
+#################
+
+cat <<EOF > /home/mike/testkey
+ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKgzf3yRLgztIX0GL5uJYSmsudJdgeGK4tXdt94g+quW mike@localhost
+EOF
+cat /home/mike/testkey |  "mkdir -p ~/.ssh/ && cat - >> ~/.ssh/authorized_keys && /bin/chmod 600 ~/.ssh/authorized_keys && /bin/chmod 700 ~/.ssh/"
+sed -i 's/#Port 22/Port 2022/g' /etc/ssh/sshd_config
+sed -i 's/#PubkeyAuthentication/PubkeyAuthentication/g' /etc/ssh/sshd_config
+sed -i 's/#ClientAliveInterval 0/ClientAliveInterval 60/g' /etc/ssh/sshd_config
+sed -i 's/#ClientAliveCountMax 3/ClientAliveCountMax 240/g' /etc/ssh/sshd_config
+
+
 ################
 # set timezone #
 ################
