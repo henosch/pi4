bd698ad641fcff966614cbd1a48298148e0e7b58
henosch <59366586+henosch@users.noreply.github.com>
2021-05-21 18:18:46 +0200

    update 02_install.sh

--- 02_install.sh
+++ 02_install.sh
@@ -513,6 +513,34 @@ sh /etc/pihole/myblocklist.sh
 curl -sSL https://install.pi-hole.net | bash /dev/stdin --unattended --disable-install-webserver
 usermod -a -G pihole www-data
 
+# Statistics 
+cat <<EOF > /etc/pihole/uniq_urls.sh
+#!/bin/bash
+# Check all lists for unique ones and show the percentage
+
+[ "$UID" -eq 0  ] || exec sudo bash "$0" "$@"
+
+cd /etc/pihole/
+cat *.domains | sort | uniq -u > all_adlist_urls_sorted_unique.txt
+
+# Result
+T1=$(cat *.domains | wc -l)
+T2=$(cat all_adlist_urls_sorted_unique.txt | wc -l)
+S=$(python -c "p = $T2 / $T1 * 100; print(p)")
+printf "URLs total:		%10d\n" $T1
+printf "URLs unique:	%10d\n" $T2
+printf "Percentage:	  %8.1f %%\n" $S
+EOF
+
+# useful commands
+# sqlite3 /etc/pihole/gravity.db "SELECT domain FROM vw_blacklist;"
+# sqlite3 /etc/pihole/gravity.db "SELECT domain FROM vw_whitelist;"
+# sqlite3 /etc/pihole/gravity.db "SELECT domain FROM vw_regex_blacklist;"
+# sqlite3 /etc/pihole/gravity.db "SELECT domain FROM vw_regex_whitelist;"
+# sqlite3 /etc/pihole/gravity.db "SELECT address FROM adlist;"
+# sqlite3 /etc/pihole/gravity.db ".schema domainlist"
+# sqlite3 /etc/pihole/gravity.db ".schema"
+
 
 #######################
 # install rpi monitor #
