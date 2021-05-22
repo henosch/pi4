bed579cc1ca861a5f8f8f68b733a27f979e75385
henosch <59366586+henosch@users.noreply.github.com>
2021-05-23 00:00:39 +0200

    Update 01_install.sh

--- 01_install.sh
+++ 01_install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/bash
 # automatic root
 [ "$UID" -eq 0 ] || exec sudo bash "$0" "$@"
 
@@ -14,6 +14,7 @@
 sed -i 's/NOPASSWD/PASSWD/g' /etc/sudoers.d/010_pi-nopasswd
 echo "mike ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers.d/010_pi-nopasswd
 service sudo restart
+
 
 #################
 # sshd settings #
