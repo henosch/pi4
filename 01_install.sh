f96263856c44d2f57d6f0ceeb3a699cfe8fbcd23
henosch <59366586+henosch@users.noreply.github.com>
2021-05-22 14:48:01 +0200

    update 01_install.sh

--- 01_install.sh
+++ 01_install.sh
@@ -137,7 +137,7 @@ apt install apache2 mariadb-server zip unzip build-essential \
   libmariadb-dev-compat libmariadb-dev libapache2-mod-security2 \
   php-apcu imagemagick php-imagick strace samba smbclient locate libsqlite3-dev \
   python3-pip python3-cffi nodejs vim lynx youtube-dl byobu ranger wajig \
-  awstats libgeo-ip-perl libgeo-ipfree-perl -y
+  awstats libgeo-ip-perl libgeo-ipfree-perl modsecurity-crs-y
   
 # config wajig
 ln -s /usr/bin/wajig /usr/bin/apt2
