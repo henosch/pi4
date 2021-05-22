0a6f9d0041bd7fc225fd73b4bf29a6f2727136e5
henosch <59366586+henosch@users.noreply.github.com>
2021-05-22 05:33:15 +0200

    update 02_install.sh

--- 02_install.sh
+++ 02_install.sh
@@ -38,8 +38,7 @@ apt install php7.4-fpm php7.4-gd php7.4-mysql php7.4-curl php7.4-xml \
   php7.4-zip php7.4-intl libapache2-mod-php7.4 php7.4-mbstring php7.4-json \
   php7.4-bz2 php7.4 php7.4-cli php7.4-common php7.4-sqlite3 php7.4-bcmath php7.4-gmp
 
-systemctl enable php5.6-fpm php7.0-fpm php7.3-fpm
-systemctl enable php7.4-fpm
+systemctl enable php5.6-fpm php7.0-fpm php7.3-fpm php7.4-fpm
 
 # standard php version for apache is 7.0
 a2dismod php7.3 php5.6 php7.4
